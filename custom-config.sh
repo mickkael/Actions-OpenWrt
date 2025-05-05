0cc02000daf957586fd909692552473ee5c58948
mickkael <19755421+mickkael@users.noreply.github.com>
2025-05-05 21:43:29 +0800

    Update custom-config.sh

--- custom-config.sh
+++ custom-config.sh
@@ -8,25 +8,8 @@
 
 echo 'CONFIG_TARGET_mediatek=y' >> $BUILD_ROOT/.config
 echo 'CONFIG_TARGET_mediatek_filogic=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_TARGET_mediatek_filogic_DEVICE_bananapi_bpi-r3-mini=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_glinet_gl-mt6000=y' >> $BUILD_ROOT/.config
 echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=4096' >> $BUILD_ROOT/.config
-
-# Lanuage Setting
-echo 'CONFIG_LUCI_LANG_zh_Hans=y' >> $BUILD_ROOT/.config
-
-# Users & Groups Support
-echo 'CONFIG_PACKAGE_shadow-utils=y' >> $BUILD_ROOT/.config
-
-# Disk Support
-echo 'CONFIG_PACKAGE_kmod-nvme=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_fdisk=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_block-mount=y' >> $BUILD_ROOT/.config
-
-# Add OpenClash
-echo '# CONFIG_PACKAGE_dnsmasq is not set' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_luci-compat=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_wget-ssl=y' >> $BUILD_ROOT/.config
 
 # Add Docker
 echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> $BUILD_ROOT/.config
