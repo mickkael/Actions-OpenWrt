ff23b2a1c6f808648d4b53c33dca4f9b621fb9b9
mickkael <19755421+mickkael@users.noreply.github.com>
2025-05-05 21:47:46 +0800

    Update custom-config.sh

--- custom-config.sh
+++ custom-config.sh
@@ -11,19 +11,6 @@ echo 'CONFIG_TARGET_mediatek_filogic=y' >> $BUILD_ROOT/.config
 echo 'CONFIG_TARGET_DEVICE_mediatek_filogic_DEVICE_glinet_gl-mt6000=y' >> $BUILD_ROOT/.config
 echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=4096' >> $BUILD_ROOT/.config
 
-# Add Docker
-echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_dockerd=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_docker-compose=y' >> $BUILD_ROOT/.config
-
 # Add other apps
-echo 'CONFIG_PACKAGE_luci=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_luci-ssl=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_PACKAGE_syslog-ng=y' >> $BUILD_ROOT/.config
 
-echo 'CONFIG_PACKAGE_luci-app-wol=y' >> $BUILD_ROOT/.config
-
-echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_ariang=y' >> $BUILD_ROOT/.config
-
-echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_luci-app-upnp=y' >> $BUILD_ROOT/.config
